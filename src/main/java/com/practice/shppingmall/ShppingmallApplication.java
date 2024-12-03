9a8f8ede794d8b69f68e600fa18fa2a03ec0a5d2
mn8467 <mn8467@naver.com>
2024-12-03 15:49:04 +0900

    initialization

---
+++ src/main/java/com/practice/shppingmall/ShppingmallApplication.java
@@ -0,0 +1,13 @@
+package com.practice.shppingmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShppingmallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShppingmallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/practice/shppingmall/ShppingmallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.shppingmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShppingmallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
