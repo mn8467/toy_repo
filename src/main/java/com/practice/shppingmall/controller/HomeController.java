b686cf0da1379c21a4c82a6bf66a8fb31ffe0044
Hojin Yoo <hojiny@fpt.com>
2024-12-04 11:21:49 +0900

    add thymeleaf template

--- src/main/java/com/practice/shppingmall/controller/HomeController.java
+++ src/main/java/com/practice/shppingmall/controller/HomeController.java
@@ -1,17 +1,21 @@
 package com.practice.shppingmall.controller;
 
+import com.practice.shppingmall.service.HomeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Slf4j
 public class HomeController {
-    
+
+    @Autowired
+    private HomeService homeService;
 
     @RequestMapping(value = "/")
     public String home() {
 
-        return "home";
+        return "pages/home";
     }
 }
